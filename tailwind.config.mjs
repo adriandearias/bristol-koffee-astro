601f16518becb044de9e5741e3878810db0a24c6
Adrian Cano Arias <adriandearias@Adrians-MacBook-Air.local>
2024-04-28 11:31:49 +0200

    centerd Header, added custom Colors palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,13 @@ export default {
 	darkMode: 'class',
 	theme: {
 		extend: {},
+		colors: {
+			customYellow: '#f6c645',
+			customGray: '#f6f4ef',
+			customBlack: '#3a3933',
+			customWhite: '#ffffff',
+			customBrown: '#2e2d26'
+		},
 	},
 	plugins: [],
 }
