94e258d636aaff2e8b64814969b353ae342640c2
Adrian Cano Arias <adriandearias@Adrians-MacBook-Air.local>
2024-04-28 13:34:16 +0200

    added footer & changd bg img & colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-		colors: {
-			customYellow: '#f6c645',
-			customGray: '#f6f4ef',
-			customBlack: '#3a3933',
-			customWhite: '#ffffff',
-			customBrown: '#2e2d26'
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+    colors: {
+      customYellow: "#f6c645",
+      customGray: "#f6f4ef",
+      customBlack: "#3a3933",
+      customWhite: "#ffffff",
+      customBrown: "#2e2d26",
+    },
+  },
+  plugins: [],
+};
