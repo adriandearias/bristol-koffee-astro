597c3d29d05becaa11f3f967ddf6d0a743979a91
adriandearias <deariasadrian@gmail.com>
2024-06-07 13:53:27 +0200

    responsive

---
+++ src/data/reviewsData.js
@@ -0,0 +1,68 @@
+export const reviews = [
+  {
+    author_name: "Elena Carmona Rodríguez",
+    text: "Por fin una buena cafetería en Viladecans!!! Café de especialidad buenísimo, también probé la limonada casera, súper rica!!! Tienen tanto opciones dulces como saladas, sin duda un buen sitio para desayunar o merendar. ¡100% recomendable!",
+    rating: 5,
+    url: "https://maps.app.goo.gl/gCwTXdsryFo9dmD16",
+    photo_url:
+      "https://res.cloudinary.com/dtmrroz8t/image/upload/v1716882727/Bristol%20Koffee/rv1-min_arja6y.jpg",
+  },
+  {
+    author_name: "Sonia L.",
+    text: "Me encanta, os deseo lo mejor y gracias por el café de especialidad 🙂…",
+    rating: 5,
+    url: "https://maps.app.goo.gl/BZRD1jTayF41s1D18",
+  },
+  {
+    author_name: "Alexandre Arasa",
+    text: "La calidad del café y la atención son espectaculares. Por no hablar de los Cinnamon Rolls y las galletas que están buenísimas. Yo creo que es la única cafetería para los amantes del café en Viladecans. Volveremos sin dudarlo.",
+    rating: 5,
+    url: "https://maps.app.goo.gl/3gZvFJiVu945VZuk7",
+    photo_url:
+      "https://res.cloudinary.com/dtmrroz8t/image/upload/v1716888686/Bristol%20Koffee/bristolKoffee_review2_msuojv.jpg",
+  },
+  {
+    author_name: "Dámaris TL",
+    text: "Me ha encantado ir! El trato y el servicio excelente, la atención rápida y las chicas son muy amables y atentas. El local es muy chulo, moderno y con personalidad. Se respira muy buen ambiente familiar. Puedes tomar dentro o fuera, que también hay varias mesas. El café es de especialidad, muy buena calidad y riquísimo! Y que decir de la comida, tanto las tostadas, que me pedí dos, a cual más rica! Y la bollería, pastas, se nota que son de calidad y muy ricas. Volveremos más de una vez!!",
+    rating: 5,
+    url: "https://maps.app.goo.gl/u9YgiqRF4sphBaMw7",
+    photo_url:
+      "https://res.cloudinary.com/dtmrroz8t/image/upload/v1716888686/Bristol%20Koffee/bristolKoffee_review3_ckq7ip.jpg",
+  },
+  {
+    author_name: "Marta Sandoval Flores",
+    text: "La mejor cafetería en la zona de Viladecans. Buen cafe y amplía carta de zumos y tes. Por no hablar de la parte dulce 🤤. Sin duda volveremos.",
+    rating: 5,
+    url: "https://maps.app.goo.gl/Ytd1G6t2u4BJnyF46",
+    photo_url:
+      "https://res.cloudinary.com/dtmrroz8t/image/upload/v1716888686/Bristol%20Koffee/bristolKoffee_review4_cw1rch.jpg",
+  },
+  {
+    author_name: "Mariona Manchon Quesada",
+    text: "Un lugar precioso, una atención perfecta y la comida buenísima!!",
+    rating: 5,
+    url: "https://maps.app.goo.gl/exn9G2SGno4P2x428",
+  },
+  {
+    author_name: "Alicia Vinader",
+    text: "Ya era hora que abriese en Viladecans una cafetería con café y productos de los buenos. Tienen unas tostadas y unas pastas súper ricas también! La atención es de 10! Nuevo sitio donde merendar🤭",
+    rating: 5,
+    url: "https://maps.app.goo.gl/jxtVgHbyjxKGQngy5",
+    photo_url:
+      "https://res.cloudinary.com/dtmrroz8t/image/upload/v1716888686/Bristol%20Koffee/bristolKoffee_review5_kvr8qm.jpg",
+  },
+  {
+    author_name: "Juan Lopez",
+    text: "Por fin una cafetería en la Rambla con personalidad y muy buena calidad que se sale de lo común. El café buenísimo. Enhorabuena de verdad",
+    rating: 5,
+    url: "https://maps.app.goo.gl/YJhpx1JS7aPcz79i7",
+  },
+  {
+    author_name: "Laia Juarez",
+    text: "Cafetería dónde te puedes tomar un buen café. La atención ha sido rápida y las chicas son muy amables.",
+    rating: 5,
+    url: "https://maps.app.goo.gl/h8Pqw9NPjxVwJ84A9",
+    photo_url:
+      "https://res.cloudinary.com/dtmrroz8t/image/upload/v1716888687/Bristol%20Koffee/bristolKoffee_review6_gaoicp.jpg",
+  },
+];
